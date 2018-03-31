f1578ac28239c2e05d4095e6ac5a1db332a5b5a0
Hexx2bin <anthonystrike02@gmail.com>
2018-03-30 19:55:49 -0500

    nuevo main sin los comentarios

--- main.cpp
+++ main.cpp
@@ -1,27 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: anthony
- *
- * Created on March 18, 2018, 3:59 PM
- */
-
 #include <iostream>
 #include <Square.h>
 
 using namespace std;
 
-/*
- * 
- */
 int main() 
 {
-    Square _mySquare(4);
+    Square _mySquare(5);
     cout << "Area: " << _mySquare.GetArea() << endl;
     cin.get();
     return 0;
